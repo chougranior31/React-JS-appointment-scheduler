ea2cdc547942065de9bd3b2f8020108db45b1822
Chougrani Sidi Mohammed <chougranior31@gmail.com>
2022-12-22 19:15:58 +0100

    seconde

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,41 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Agenda from "../../Components/Agenda/Agenda";
 
+import { useEffect } from "react";
+import { getAppoitment } from "../../redux/features/Agenda/AgendaSlice";
+import { Fragment } from "react";
+
 export default function Home() {
-  return <div>Home</div>;
+
+
+  const dispatch = useDispatch()
+  const { appoitment } = useSelector((state) => state.agenda)
+
+
+
+  useEffect(() => {
+    dispatch(getAppoitment())
+  },  [dispatch]);
+
+
+
+
+
+
+
+  return (
+  
+    <Fragment>  
+      <div className="items">
+        {appoitment && appoitment.map((el, i) => 
+          <div key={i} className="item">
+            <p>{el.name}</p>
+          </div>
+        )}
+      </div>
+    </Fragment>
+  
+  
+);
 }
--- src/redux/features/Agenda/AgendaSlice.js
+++ src/redux/features/Agenda/AgendaSlice.js
@@ -3,12 +3,33 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 
 
+export const getAppoitment = createAsyncThunk('agenda/getAppoitment',
+  async (_, thunkAPI) => {
+    const {rejectWithValue} = thunkAPI;
+    try {
+      const res = await fetch('/api/appointment/listappoitment/', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json; charset=utf',
+          "Accept": "application/json",
+        },
+      });
+      const data = await res.json();
+      return data
+    } catch (error) {
+        return rejectWithValue(error.message);
+    }
+  }
+)
+
+
 
 
 
 const AgendaSlice = createSlice({
     name: 'agenda',
     initialState : {
+        appoitment: [],
         agenda: '',
         isLoading: false,
         error: null,
@@ -16,6 +37,19 @@ const AgendaSlice = createSlice({
     reducers: {
     },
     extraReducers: {
+      // get appoitment
+    [getAppoitment.pending]: (state, action) => {
+        state.isLoading = true;
+        state.error = null;
+    },
+    [getAppoitment.fulfilled]: (state, action) => {
+        state.isLoading = false;
+        state.appoitment = action.payload;
+    },
+    [getAppoitment.rejected]: (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+    },
     },
 });
 
